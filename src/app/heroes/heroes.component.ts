050877ab8c74fd476c0f230a10ec9975a7277d79
ozaki <fs65ugumori@yahoo.co.jp>
2018-08-10 19:49:49 +0900

    add index heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from './mock-heroes';
 import { Hero } from './Hero';
 
 @Component({
@@ -7,14 +8,16 @@ import { Hero } from './Hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
+
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
