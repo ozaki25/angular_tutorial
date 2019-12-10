032bb5083ef6fb72540532489c537795e5bef444
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-12-10 12:34:28 +0900

    add detail route

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,14 +4,17 @@ import { MessageService } from './message.service';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
   constructor(private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
